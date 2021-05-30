3248adc6a79b4ff4e24a40b5773bbce2e5690bee
abhi <@abhi_bops>
2021-05-30 16:23:23 +0530

    Removed quiet option and parsing and storing the times of each ping packet - for stats measurement

--- src/pingx.py
+++ src/pingx.py
@@ -11,17 +11,20 @@ class Ping(object):
         self.parse_output()
 
     def run_ping(self):
-        command = 'ping -q -c {} {}'.format(self.count, self.source)
+        command = 'ping -c {} {}'.format(self.count, self.source)
         result = run_command(command)
         self.ping_raw = result[0]
 
     def parse_output(self):
+        times = []
         for line in self.ping_raw.split('\n'):
+            #100 packets transmitted, 59 packets received, +2 duplicates, 41.0% packet loss
             if 'packets transmitted' in line:
                 info = line.split()
                 sent = int(info[0])
                 recv = int(info[3])
                 loss = round(float((sent-recv)/sent), 2)
+            #round-trip min/avg/max/stddev = 46.942/89.985/244.588/41.762 ms
             elif 'round-trip' in line:
                 info = line.split()
                 stats = info[3].split('/')
@@ -29,11 +32,19 @@ class Ping(object):
                 rtt_avg = float(stats[1])
                 rtt_max = float(stats[2])
                 rtt_stddev = float(stats[3])
+            #To Handle these lines
+            # 64 bytes from 1.1.1.1: icmp_seq=40 ttl=253 time=95.450 ms
+            # 64 bytes from 1.1.1.1: icmp_seq=40 ttl=253 time=98.275 ms (DUP!)
+            # 64 bytes from 1.1.1.1: icmp_seq=41 ttl=253 time=96.287 ms
+            elif 'bytes from' in line and '(DUP!)' not in line:
+                time = line.split()[-2].split('=')[-1]
+                times.append(time)
         self.ping_results = {'sent': sent,
                              'recv': recv,
                              'loss': loss,
                              'min': rtt_min,
                              'avg': rtt_avg,
                              'max': rtt_avg,
-                             'stddev': rtt_stddev}
+                             'stddev': rtt_stddev,
+                             'times': times}
 
